63bad9b12cebd162e07fba6e3881d427a8b6f703
Ronaldo Carvalho <ronaldocarvalho798@gmail.com>
2025-05-12 21:59:45 -0300

    add header-bg/ script

--- assets/script/engine.js
+++ assets/script/engine.js
@@ -10,3 +10,12 @@ const observer = new IntersectionObserver((entries) => {
 const elementsToObserve = document.querySelectorAll('.description-section, .contain, .section-description');
 elementsToObserve.forEach(el => observer.observe(el));
 
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0 || document.documentElement.scrollTop > 0) {
+    console.log("O scroll não está mais no topo da página!");
+    document.querySelector('.header-bg ').classList.add('visib');
+  } else {
+    console.log("O scroll voltou para o topo.");
+    document.querySelector('.header-bg ').classList.remove('visib'); // <---- Correção aqui
+  }
+});
